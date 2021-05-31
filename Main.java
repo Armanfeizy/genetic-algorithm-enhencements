cead945828adedf27651869fd5561323255328fa
Armanfeizy <59360853+Armanfeizy@users.noreply.github.com>
2021-05-31 13:10:18 +0430

    Add source

---
+++ Main.java
@@ -0,0 +1,12 @@
+package ai;
+
+import ai.uni2.Runner;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Runner.run(null, "tmp/levels/level9.txt");
+//        Runner.run(PathFinderAlgorithm.BBFS, "test5.txt");
+    }
+}
---
+++ uni2/FitnessModel.java
@@ -0,0 +1,4 @@
+package ai.uni2;
+
+public enum FitnessModel {
+}
